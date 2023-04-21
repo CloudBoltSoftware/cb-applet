28f2ed44a4ae91f39792647eb5e38ce24e0c20e9
Orvis Evans <oevans@cloudbolt.io>
2023-04-21 16:55:52 -0700

    Update for Vue3

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import vue from "@vitejs/plugin-vue";
-import camelCase from "camelcase";
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-// The name of the applet from package.json in PascalCase.
-const PACKAGE_NAME = process.env.npm_package_name;
-const pascalPackageName = camelCase(PACKAGE_NAME, {
-  pascalCase: true,
-});
+/**
+ * When building the vue library and keeping vue as an external dependency,
+ * the import statements in the vue files are not resolved correctly. Specifically,
+ * we attempt to import 'createElementVNode' from 'vue' but it is not found.
+ * In vue3, that function is simply called 'h'.
+ *
+ * This is an absolutely terrible hack to fix the issue for now. There is some
+ * subtle version mismatch, bug, or interplay between vite, vue, and rollup that
+ * does this. It's only a problem when vue is external. 🤷
+ */
+function vueImportCorrector() {
+  return {
+    name: "vue-import-corrector",
+    transform(src, id) {
+      if (id.endsWith(".vue")) {
+        console.log("TRANSFORMING");
+        return src.replace(/createElementVNode/g, "h");
+      }
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +33,8 @@ export default defineConfig({
     // Resolves node modules. See for more information:
     // https://github.com/rollup/plugins/tree/master/packages/node-resolve
     nodeResolve(),
+
+    vueImportCorrector(),
   ],
 
   build: {
@@ -27,20 +43,13 @@ export default defineConfig({
       // ES Modules are the browser's native module format.
       // This lets us use the native import() function to load this module.
       formats: ["es"],
-      // name: pascalPackageName,
-      entry: resolve(__dirname, "src/TheApplet.vue"),
-      fileName: "main",
+      entry: "src/TheApplet.vue",
+      fileName: (format) => `main.${format}.js`,
     },
 
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled into your component.
       external: ["vue"],
-      // output: {
-      //   // Provide global variables to use in the UMD build for externalized deps
-      //   globals: {
-      //     vue: "Vue",
-      //   },
-      // },
     },
   },
 });
