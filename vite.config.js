90e2f9c1894ed5772fdc8f9456a141695500fab0
Orvis Evans <oevans@cloudbolt.io>
2023-05-22 13:00:33 -0700

    Fix postBuildScriptRunner
    Swap from exec to spawn and changed hook to closeBundle to make sure the entire bundle is written before the script runs.

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,33 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { exec } from "child_process";
+import { spawn } from "child_process";
 
 /**
  * Run a script after each build. This works on all changes with build --watch too.
  * @param {object} options
  * @param {string} options.script terminal script to run after each build
+ * @param {string} [options.apply='build'] vite plugin apply option
+ * @param {string} [options.enforce='post'] vite plugin enforce option
+ * @param {string} [options.hook='closeBundle'] vite plugin hook option
  */
 function postBuildScriptRunner(options) {
   return {
     name: "post-build-plugin",
-    apply: "build",
-    async writeBundle() {
-      exec(options.script, (error, stdout, stderr) => {
-        if (error) console.error(error.message);
-        if (stderr) console.error(stderr);
-        console.log(stdout);
-      });
+    apply: options.apply || "build",
+    enforce: options.enforce || "post",
+    // By default, run on the closeBundle hook, which is after each build has written
+    // its files to disk. This ensures all the `dist` files are available to the script.
+    async [options.hook || "closeBundle"]() {
+      console.log(`Running post build script: "${options.script}"`);
+      const [command, ...args] = options.script.split(" ");
+      const child = spawn(command, args, { stdio: "inherit" });
+      child.on("error", (error) => console.error(error));
+      child.on("close", (code) =>
+        console.log(
+          `post build script "${options.script}" exited with code ${code}`
+        )
+      );
     },
   };
 }
@@ -60,9 +70,25 @@ export default defineConfig({
       external: [
         "vue",
         "vuetify",
+        "@syncfusion/ej2-base",
+        "@syncfusion/ej2-buttons",
+        "@syncfusion/ej2-calendars",
+        "@syncfusion/ej2-compression",
+        "@syncfusion/ej2-data",
+        "@syncfusion/ej2-dropdowns",
+        "@syncfusion/ej2-excel-export",
+        "@syncfusion/ej2-file-utils",
+        "@syncfusion/ej2-grids",
+        "@syncfusion/ej2-icons",
+        "@syncfusion/ej2-inputs",
+        "@syncfusion/ej2-lists",
+        "@syncfusion/ej2-navigations",
+        "@syncfusion/ej2-notifications",
+        "@syncfusion/ej2-pdf-export",
+        "@syncfusion/ej2-popups",
+        "@syncfusion/ej2-splitbuttons",
         "@syncfusion/ej2-vue-base",
         "@syncfusion/ej2-vue-grids",
-        "@syncfusion/ej2-layouts",
       ],
     },
   },
