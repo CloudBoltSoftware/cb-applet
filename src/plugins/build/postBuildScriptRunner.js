bc75e9506f4186664a5ae720a7e8baf106ac95d7
Orvis Evans <oevans@cloudbolt.io>
2023-06-22 10:21:16 -0700

    Change default build hook to writeBundle

--- src/plugins/build/postBuildScriptRunner.js
+++ src/plugins/build/postBuildScriptRunner.js
@@ -10,16 +10,16 @@ import { spawn } from "child_process";
  * @param {string} options.script terminal script to run after each build
  * @param {string} [options.apply='build'] vite plugin apply option
  * @param {string} [options.enforce='post'] vite plugin enforce option
- * @param {string} [options.hook='closeBundle'] vite plugin hook option
+ * @param {string} [options.hook='writeBundle'] vite plugin hook option
  */
 export function postBuildScriptRunner(options) {
   return {
     name: "post-build-plugin",
     apply: options.apply || "build",
     enforce: options.enforce || "post",
-    // By default, run on the closeBundle hook. This is after each build has written
+    // By default, run on the writeBundle hook. This is after each build has written
     // its files to disk. This ensures all the `dist` files are available to the script.
-    async [options.hook || "closeBundle"]() {
+    async [options.hook || "writeBundle"]() {
       console.log(`Running post build script: "${options.script}"`);
       const [command, ...args] = options.script.split(" ");
       const child = spawn(command, args, { stdio: "inherit" });
