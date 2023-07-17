a254ba1c9cf7d0179ed1914339449f6511d811dd
tropotorres <105660893+tropotorres@users.noreply.github.com>
2023-07-17 14:30:03 -0400

    [ENG-25279] Inbound Webhook example (#9)
    * [ENG-25279] Inbound Webhook example
    
    * [ENG-25279] added component for api examples and button trigger. Slight modification of overall appearance
    
    * [ENG-25279] updated text

---
+++ src/NestedExamples.vue
@@ -0,0 +1,77 @@
+<template>
+    <!-- 
+    VCard comes from Vuetify: https://vuetifyjs.com/en/components/cards/
+    and provide a great way to wrap related content together
+    max-width and the margin class "mb-2" provide simple css customization
+  -->
+  <VCard
+    :loading="apiResponseLoading"
+    title="API Examples"
+    max-width="25%"
+    class="mb-2">
+    <VCardActions>
+      <!-- API Example for Inbound Webhooks -->
+      <SimpleButton 
+        text="Fetch Inbound Webhook" 
+        disabled-text="Update Webhook ID to enable" 
+        :button-action="fetchInboundWebHook" 
+        :disabled="!Boolean(webHookId)"/>
+    </VCardActions>
+  </VCard>
+</template>
+
+<script setup>
+/**
+ * Here we are using our custom SimpleButton in multiple iterations
+ */
+import { ref } from "vue";
+import SimpleButton from "./SimpleButton.vue";
+
+/**
+ * @typedef {object} Props
+ * @property {ReturnType<import("@cloudbolt/js-sdk").createApi>} Props.api - The authenticated API instance
+ */
+/** @type {Props} */
+const props = defineProps({
+  api: {
+    type: Object,
+    required: true,
+  },
+});
+
+/**
+ * Similar to the first NestedComponent, we'll use the `api` prop to make a variety of API calls.
+ * We'll also reactively store the loading state while each call is fetching. 
+ * Each call will be handled separately and triggered by their individual buttons.
+ */
+const apiResponseLoading = ref(false);
+
+// Use the ID provided by the URL of your webhook.
+// Ex. /api/v3/cmp/inboundWebHooks/IWH-f7gfst8e/run/ results in webHookId = 'IWH-f7gfst8e'
+// Using the ref allows disabling the SimpleButton component when value is not set
+const webHookId = ''
+async function fetchInboundWebHook() {
+  try {
+    // Webhook ID must be correctly set for this function to work.
+    // Optional Parameters Object 
+    const options = {value: 'Your parameters'}
+    apiResponseLoading.value = true;
+    // Another Async call to the api
+    // Use `.then()` and `.catch()` to handle the response
+    // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Promises
+    const webHook = await props.api.v3.cmp.inboundWebHooks.runGet(webHookId, options)
+    // Logging the response for this example
+    console.log(webHook)
+    apiResponseLoading.value = false;
+  } catch (error) {
+    // When using API calls, it's a good idea to catch errors and meaningfully display them.
+    // In this case, we'll just log the error to the console.
+    console.error(error);
+  }
+}
+
+</script>
+
+<!-- These styles won't affect anything outside this component -->
+<style scoped>
+</style>
---
+++ src/SimpleButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- 
+    VBtn comes from Vuetify: https://vuetifyjs.com/en/components/buttons/ 
+  
+    The `@click`s are a examples of how you bind js functions to events on elements.
+    See more: https://vuejs.org/guide/essentials/event-handling.html
+  -->
+  <VBtn :disabled="disabled" :text="buttonText" @click="buttonAction" class="d-block my-1" variant="outlined"></VBtn>
+</template>
+
+<script setup>
+import { computed } from "vue";
+/**
+ * @typedef {object} Props
+ * @property {function} Props.buttonAction - Function to run when button clicked
+ * @property {string} Props.text - String for button text
+ * @property {string} Props.disabledText - String for button text when disabled
+ * @property {boolean} Props.disabled - Boolean to toggle button functionality
+ */
+/** @type {Props} */
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  buttonAction: {
+    type: Function,
+    required: true
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  disabledText: {
+    type: String,
+    default: ''
+  }
+});
+
+const buttonText = computed(()=> props.disabled && props.disabledText ? props.disabledText : props.text)
+
+</script>
