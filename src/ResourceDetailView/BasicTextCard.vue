6bed8614858d188f6abc071408ddb61e93d5de5c
tropotorres <atorres@cloudbolt.io>
2023-07-24 16:37:32 -0400

    [ENG-25279] Added parameter specific card, suggested extensions list

---
+++ src/ResourceDetailView/BasicTextCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- 
+    VCard comes from Vuetify: https://vuetifyjs.com/en/components/cards/
+    and provide a great way to wrap related content together.
+    The "tonal" variant is a built-in customization to change the overall card appearance
+  -->
+  <VCard
+    :title="info.label"
+    class="mb-2"
+    variant="tonal"
+  >
+  <!-- 
+  VCardText slot sits below the header and can be considered the main body of the card
+  -->
+    <VCardText>
+    <!-- Double brackets to simply render the `info.value` -->
+    {{ info.value}}
+    </VCardText>
+  </VCard>
+</template>
+
+
+<script setup>
+/**
+ * Unlike some of the other example components which focus on specific use cases, 
+ * sometimes you will want to create generic components with reusability
+ * As long as this component recieves an `info` object that has a `label` and `value` properties
+ * it will render a consistent VCard result that can be used anywhere
+ */
+
+/**
+ * @typedef {object} Props
+ * @property {object} Props.info - The simple info object provided to the component
+/** @type {Props} */
+
+defineProps({
+  info: {
+    type: Object,
+    required: true,
+  },
+});
+
+</script>
+
+<!-- These styles won't affect anything outside this component, but will impact every iteration of this component -->
+<style scoped></style>
--- src/ResourceDetailView/ResourceDetailView.vue
+++ src/ResourceDetailView/ResourceDetailView.vue
@@ -1,10 +1,12 @@
 <template>
+  <BasicTextCard v-if="datadogParam" :info="datadogParam"/>
   <ApiExamples :api="api" :resource="context.resource" />
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { computed, defineProps } from "vue";
 import ApiExamples from "./ApiExamples.vue";
+import BasicTextCard from "./BasicTextCard.vue";
 /**
  * Recommend starting at src/MainView/MainView.vue for a more in-depth explanation of component parts
  */
@@ -16,7 +18,7 @@ import ApiExamples from "./ApiExamples.vue";
  */
 
 /** @type {Props} */
-defineProps({
+const props = defineProps({
   api: {
     type: Object,
     required: true,
@@ -29,4 +31,13 @@ defineProps({
     default: () => ({}),
   },
 });
+
+/**
+ * Leveraging information provided by the resource context to create a conditionals for an example.  
+ * You can always console.log(props.context) to see what is available to work with.
+ * This reactive value will check the resource context being passed for a custom parameter added to the resource 
+ * whose name matches the desired value of "Datadog Parameter", and then displays this information in another VCard component
+ * Custom Parameters can be created at `admin/customfields/` then added to the resource on the Parameters tab
+ */
+const datadogParam = computed(() => props.context.resource?.groupedAttributesMap?.parameters?.attributes.filter((each) => each.label === "Datadog Parameter")[0])
 </script>
